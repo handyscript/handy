d1d6dc6f238de2d4bb17372ee89bce56c13cbb73
whybe7 <whybe7@hotmail.com>
2023-06-02 12:40:59 +0100

    Number human string representation

--- packages/handy/index.ts
+++ packages/handy/index.ts
@@ -1,6 +1,7 @@
 import "./lib/array";
 import "./lib/string";
 import "./lib/math";
+import "./lib/number";
 import HOperators from "./lib/operators";
 import HashMap from "./lib/hashmap";
 import Matrix from "./lib/matrix";
