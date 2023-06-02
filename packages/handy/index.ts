3795125c2f998ab2f93b3005c18509bd18dc8e63
whybe7 <whybe7@hotmail.com>
2023-06-02 12:32:40 +0100

    Implmenting martix manipulation in javascript

--- packages/handy/index.ts
+++ packages/handy/index.ts
@@ -3,8 +3,10 @@ import "./lib/string";
 import "./lib/math";
 import HOperators from "./lib/operators";
 import HashMap from "./lib/hashmap";
+import Matrix from "./lib/matrix";
 
 export {
     HOperators,
-    HashMap
+    HashMap,
+    Matrix,
 };
